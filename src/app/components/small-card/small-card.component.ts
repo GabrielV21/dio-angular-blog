abd21487350b52cfb4f7cb9c17d6429b72c952f7
Gabriel <gabrielvasconcelosv21@gmail.com>
2024-01-31 21:17:40 -0300

    Content created + dynamic fake data

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SmallCardComponent implements OnInit {
 
   @Input()
+  Id:string=""
+  @Input()
   photoCover:string = ""
   @Input()
   cardTitle:string = ""
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo":"https://sm.ign.com/ign_br/news/r/rick-and-m/rick-and-morty-season-7-gets-rick-heavy-first-look-fall-rele_d9gq.jpg",
+        "title":"Estão tentando nos calar!",
+        "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam obcaecati impedit aut! Debitis, nisi alias! Nisi molestias officia, praesentium vel, excepturi dolorem, omnis harum odio molestiae ipsam eos quo nam!"
+    },
+    {
+        "id": "2",
+        "photo":"https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1686,w_3000,x_0,y_0/dpr_1.5/c_limit,w_1600/fl_lossy,q_auto/v1588568507/200503-leon-rick-morty-ease_f8zkih",
+        "title":"Aulas de direção",
+        "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam obcaecati impedit aut! Debitis, nisi alias! Nisi molestias officia, praesentium vel, excepturi dolorem, omnis harum odio molestiae ipsam eos quo nam!"
+    },
+    {
+        "id": "3",
+        "photo":"https://tm.ibxk.com.br/2022/11/08/08084332788026.jpg?ims=1200x675",
+        "title":"Fogo no Cerrado",
+        "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam obcaecati impedit aut! Debitis, nisi alias! Nisi molestias officia, praesentium vel, excepturi dolorem, omnis harum odio molestiae ipsam eos quo nam!"
+    },
+    {
+        "id": "4",
+        "photo":"https://imageio.forbes.com/specials-images/imageserve/61004984b3f2e02234907f95/Rick-and-Morty-are-no-longer-voiced-by-Justin-Roiland--but-still-sound-the-same/960x0.jpg?format=jpg&width=960",
+        "title":"Chuva Ácida",
+        "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam obcaecati impedit aut! Debitis, nisi alias! Nisi molestias officia, praesentium vel, excepturi dolorem, omnis harum odio molestiae ipsam eos quo nam!"
+    }
+    
+]
