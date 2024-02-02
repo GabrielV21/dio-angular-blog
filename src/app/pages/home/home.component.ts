de2459cf3ef3b56bb434646049a77293e0440ea0
Gabriel <gabrielvasconcelosv21@gmail.com>
2024-02-01 21:01:06 -0300

    HomePage 'dynamic' data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  id:string[]=["1","2","3","4"]
+  title:string[]=[]
+  photoCover:string[]=[]
+  description:string[]=[]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.setValues()
+  }
+
+  setValues(){
+    for(let i = 0;i<dataFake.length;i++) {
+      const result = dataFake.filter(article => article.id == this.id[i])[0]
+      this.title[i] = result.title
+      this.photoCover[i] = result.photo
+      this.description[i] = result.description
+    }
   }
 
 }
